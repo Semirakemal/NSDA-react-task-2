fa6d0f0aea90e6f6d8942745d2e3b7d1927c298c
Semira kemal <Ksemira225@gmail.com>
2025-08-24 09:14:37 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "./style.css";
+import Card from './Components/Card.jsx';
+
+
+export default function App() {
+  return (
+    <>
+    <Card title="ph"  dueDate={2020-24-8} />
+    <Card title="chem" dueDate={2022-24-8} />
+    <Card title="bi"  dueDate={2023-24-8} />
+        <Card title="c++ " dueDate={2043-24-8} />
+            <Card title="se " dueDate={2014-24-8} />
+            
+
+
+    </>  
+  );
+}
