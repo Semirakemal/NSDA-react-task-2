c1bfc722fe91503ac885976a17441a52c8468898
Semira kemal <Ksemira225@gmail.com>
2025-08-24 09:15:27 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import Nav from './Components/Nav.jsx';
 import Home  from './Components/Home.jsx';
-import App from './App.jsx';
+import App from './App';
 
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
